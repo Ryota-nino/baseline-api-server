22f6fd84a88456f6cd47b2253a04a45c2d638487
Shuta Kakishiro <kakishiro2190394@gmail.com>
2020-11-03 16:26:20 +0900

    MyActivityFactoryを作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\MyActivity;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         User::factory(100)->create();
+         User::factory(50)->create();
+         MyActivity::factory(200)->create();
     }
 }
