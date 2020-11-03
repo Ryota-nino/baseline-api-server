038cf2acab2195bc7221bcf4556bf806734b25ba
Shuta Kakishiro <kakishiro2190394@gmail.com>
2020-11-03 18:21:11 +0900

    companyをmodelを追加

---
+++ database/migrations/2020_11_03_085648_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('frigana'); # フリガナ
+            $table->string('company_name'); # 企業名
+            $table->text('business_description'); # 事業内容
+            $table->bigInteger('number_of_employees'); # 従業員数
+            $table->string('logo_path'); # ロゴのパス
+            $table->string('company_url'); # URL
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
