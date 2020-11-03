7ad54ddd9b549ba7d2eef0bafc6e94399146c2e7
Shuta Kakishiro <kakishiro2190394@gmail.com>
2020-11-03 14:10:59 +0900

    マイグレーションを追加

---
+++ database/migrations/2020_11_03_050028_create_prefectures_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrefecturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prefectures', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prefectures');
+    }
+}
