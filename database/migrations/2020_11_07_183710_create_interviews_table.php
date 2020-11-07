5fc5bcb64f7845ee47c0be38a0678a17492eba8c
K-shir0 <kakishiro2190394@gmail.com>
2020-11-08 04:00:54 +0900

    面接内容テーブルのmigrationを作成

--- database/migrations/2020_11_07_183710_create_interviews_table.php
+++ database/migrations/2020_11_07_183710_create_interviews_table.php
@@ -14,13 +14,14 @@ class CreateInterviewsTable extends Migration
     public function up()
     {
         Schema::create('interviews', function (Blueprint $table) {
+            $table->id();
             $table->bigInteger('company_information_id')->unsigned();
             $table->bigInteger('step')->unsigned();
             $table->boolean('results');
             $table->date('interview_date');
 
-            $table->foreign('company_information_id')->references('id')->on('occupational_categories');
-            $table->primary(['company_information_id', 'step']);
+            $table->foreign('company_information_id')->references('id')->on('company_information');
+            $table->unique(['company_information_id', 'step']);
         });
     }
 
