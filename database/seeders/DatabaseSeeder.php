493bec15f61e8b24fb94636c34173f5b50d906ac
K-shir0 <kakishiro2190394@gmail.com>
2020-11-08 06:17:10 +0900

    面接テーブルのseederを作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,13 +4,15 @@ namespace Database\Seeders;
 
 
 use App\Models\Company;
-use App\Models\Draft;
 use App\Models\CompanyInformation;
+use App\Models\Draft;
 use App\Models\EmploymentStatus;
 use App\Models\MyActivity;
 use App\Models\User;
+use Faker\Factory as FakerFactory;
 use Illuminate\Database\QueryException;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -48,5 +50,30 @@ class DatabaseSeeder extends Seeder
             }
         }
 
+        // 面接を50件生成
+        CompanyInformation::factory(50)
+            ->create()
+            ->each(function (CompanyInformation $companyInformation) {
+                $faker = FakerFactory::create();
+
+                // 面接のステップ1~5ランダム生成
+                for ($i = 0; $i < rand(1, 5); $i++) {
+                    $id = DB::table('interviews')->insertGetId([
+                        'company_information_id' => $companyInformation->id,
+                        'step' => ($i + 1),
+                        'results' => rand(0, 1),
+                        'interview_date' => $faker->date()
+                    ]);
+
+
+                    // 面接内容を数件追加
+                    for ($j = 0; $j < rand(1, 5); $j++) {
+                        DB::table('interview_contents')->insert([
+                            'interview_id' => $id,
+                            'content' => $faker->sentence($faker->numberBetween(30, 200))
+                        ]);
+                    }
+                }
+            });
     }
 }
