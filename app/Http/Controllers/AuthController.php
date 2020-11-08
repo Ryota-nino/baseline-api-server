3846f1236a34d895d0d713b05fa84156b1113939
K-shir0 <kakishiro2190394@gmail.com>
2020-11-08 14:57:39 +0900

    認証サンプルを追加

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        return response()->json([
+            'message' => 'Unauthorized'
+        ], 401);
+    }
+}
