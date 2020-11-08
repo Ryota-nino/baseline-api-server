a8ac6a9eeb587acfdf1670f4391d89786a8a67c4
K-shir0 <kakishiro2190394@gmail.com>
2020-11-08 13:32:09 +0900

    Laravel sanctum設定ファイルを記述

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,12 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            /**
+             *
+             * sanctumのミドルウェア
+             *
+             */
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
