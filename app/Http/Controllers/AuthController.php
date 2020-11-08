0a2b0e3b855b6659b91b5225a6547a07c67471a7
K-shir0 <kakishiro2190394@gmail.com>
2020-11-08 21:09:19 +0900

    ログイン機能を追加

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,13 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
+        $credential = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $status = 401;
+        $message = 'Unauthorized';
+
+        if (Auth::attempt($credential)) {
+            $status = 200;
+            $message = 'OK';
+        }
+
         return response()->json([
-            'message' => 'Unauthorized'
-        ], 401);
+            'message' => $message
+        ], $status);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', [AuthController::class, 'login']);
+
+/*
+ *
+ * 認証ルート(Auth)
+ *
+ *
+ *
+ * */
+Route::post('/auth/login', [AuthController::class, 'login']); // ログイン
