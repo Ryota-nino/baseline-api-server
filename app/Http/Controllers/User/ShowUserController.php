e2da48ed2493a15c976edb60a2cdf141cb0f8207
K-shir0 <kakishiro2190394@gmail.com>
2020-11-14 00:09:27 +0900

    コメントを修正

--- app/Http/Controllers/User/ShowUserController.php
+++ app/Http/Controllers/User/ShowUserController.php
@@ -18,11 +18,12 @@ class ShowUserController extends Controller
     public function __invoke(Request $request)
     {
         $user = Auth::user();
+        // 希望職種idを取得
         $desired_occupations_id = $user->desired_occupations;
 
-        // もし希望職種がnull出なければ職種名を取得して代入する
+        // もし希望職種がnull以外であれば職種名を取得して代入する
         if ($desired_occupations_id) {
-            $user->desired_occupations = User::find(1)->desired_occupation()->first('name')->name;
+            $user->desired_occupations = User::find($user->id)->desired_occupation()->first('name')->name;
         }
 
         return $user;
