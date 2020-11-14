412f0957e9127caeadb34c0724108d9de3d8d988
Ryota-nino <ryota.swimmer@icloud.com>
2020-11-15 02:39:40 +0900

    ValidationとControllerを分離

--- app/Http/Controllers/Company/RegistCompanyController.php
+++ app/Http/Controllers/Company/RegistCompanyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Company;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CompanyRequest;
 use App\Models\Company;
 use Illuminate\Http\Request;
 
@@ -14,23 +15,8 @@ class RegistCompanyController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(CompanyRequest $request)
     {
-        //validate
-        $request->validate([
-            'frigana' => [
-                'required',
-                'regex:/^[ア-ン゛゜ァ-ォャ-ョー]+$/u'
-            ],
-            'company_name' => 'required',
-            'business_description' => 'required',
-            'prefecture_id' => 'required|array',
-            'prefecture_id.*' => 'integer|between:1,47',
-            'number_of_employees' => 'required|integer',
-            'logo_path' => 'required',
-            'company_url' => 'required|url',
-        ]);
-
         $company = new Company();
         $status = 200;
         $message = 'OK';
