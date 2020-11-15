d142187f000a5339c76e2c1b92f19b287f83ee9d
K-shir0 <kakishiro2190394@gmail.com>
2020-11-15 19:07:40 +0900

    画像送信が空のときでもエラーが出ないように変更

--- app/Http/Controllers/Company/RegistCompanyController.php
+++ app/Http/Controllers/Company/RegistCompanyController.php
@@ -29,30 +29,40 @@ class RegistCompanyController extends Controller
             'prefecture_id' => 'required|array',
             'prefecture_id.*' => 'integer|between:1,47',
             'number_of_employees' => 'required|integer',
-            'logo_image' => [
-                'required',
-                'regex:/data:image\/jpeg;base64,\//'
-            ],
             'company_url' => 'required|url',
         ]);
-
-        $image = $request->logo_image;  // base64
-        $image = str_replace('data:image/jpeg;base64,', '', $image);
-        $image = str_replace(' ', '+', $image);
-        $imageName = Uuid::uuid() . '.' . 'jpeg';
-
-        //TODO サイズのバリデート
-
-        // 画像を保存
-        Storage::disk('public')->put($imageName, base64_decode($image));
-        // パスの取り出し
-        $logo_path = Storage::url($imageName);
 
         $company = new Company();
         $status = 200;
         $message = 'OK';
+        $logo_path = "";
 
-        if (!$company->fill($request->all())->fill(['logo_path' => $logo_path])->save()) {
+        $company->fill($request->all());
+
+        // 画像が送信されてきているとき;
+        if ($request->logo_image) {
+            // 画像のバリデーション
+            $request->validate([
+                'logo_image' => [
+                    'regex:/data:image\/jpeg;base64,\//'
+                ],
+            ]);
+
+            // 画像を保存してPathを取得する処理
+            $image = $request->logo_image;  // base64
+            $image = str_replace('data:image/jpeg;base64,', '', $image);
+            $image = str_replace(' ', '+', $image);
+            $imageName = Uuid::uuid() . '.' . 'jpeg';
+
+            //TODO サイズのバリデート
+
+            // 画像を保存
+            Storage::disk('public')->put($imageName, base64_decode($image));
+            // パスの取り出し
+            $logo_path = Storage::url($imageName);
+        }
+
+        if (!$company->fill(['logo_path' => $logo_path])->save()) {
             $status = 400;
             $message = 'Bad Request';
         }
