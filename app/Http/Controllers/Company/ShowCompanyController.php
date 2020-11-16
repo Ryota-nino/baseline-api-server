0a84d9fe85524a27cc15f19c2fd1e1376cebc351
K-shir0 <kakishiro2190394@gmail.com>
2020-11-16 23:56:55 +0900

    企業情報の形式を変更

--- app/Http/Controllers/Company/ShowCompanyController.php
+++ app/Http/Controllers/Company/ShowCompanyController.php
@@ -4,16 +4,14 @@ namespace App\Http\Controllers\Company;
 
 use App\Http\Controllers\Controller;
 use App\Models\Company;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class ShowCompanyController extends Controller
 {
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
      */
     public function __invoke($id)
     {
@@ -21,23 +19,10 @@ class ShowCompanyController extends Controller
 
         $status = 200;
 
-        $company = Company::findOrfail($id);
-
-        $company_data = [
-            "id" => $company->id,
-            "frigana" => $company->frigana,
-            "company_name" => $company->company_name,
-            "business_description" => $company->business_description,
-            "number_of_employees" => $company->number_of_employees,
-            "logo_path" => $company->logo_path,
-            "company_url" => $company->company_url,
-            "created_at" => $company->created_at,
-            "updated_at" => $company->updated_at,
-            "prefectures" => array_column($company->prefectures->toArray(), 'id'),
-        ];
+        $company = Company::with('prefectures:id')->findOrfail($id);
 
         return response()->json(
-            $company_data, $status
+            $company, $status
         );
     }
 }
