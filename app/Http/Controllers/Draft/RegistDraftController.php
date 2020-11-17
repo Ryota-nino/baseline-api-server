ae82999f34b5144322066efce231bd7bbeb01047
Ryota-nino <ryota.swimmer@icloud.com>
2020-11-17 13:20:14 +0900

    下書きを保存する機能を追加

---
+++ app/Http/Controllers/Draft/RegistDraftController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Draft;
+
+use App\Http\Controllers\Controller;
+use App\Models\Draft;
+use App\Http\Requests\DraftRequest;
+use Illuminate\Http\Request;
+
+class RegistDraftController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+            'content' => 'required',
+            'posted_by' => 'required|integer'
+        ]);
+
+        $draft = new Draft();
+        $status = 200;
+        $message = 'OK';
+
+        if(!$draft->fill($request->all())->save()) {
+            $status = 400;
+            $message = 'Bad Request';
+        }
+
+        return response()->json([
+            'message' => $message
+        ], $status);
+    }
+}
