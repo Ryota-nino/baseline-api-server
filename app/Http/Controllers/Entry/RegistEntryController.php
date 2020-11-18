e5c039bbb762d8d30b65f23215c47c6698c4f2a1
Ryota-nino <ryota.swimmer@icloud.com>
2020-11-18 11:58:02 +0900

    エントリーモデルのvalidateを作成

---
+++ app/Http/Controllers/Entry/RegistEntryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Entry;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegistEntryController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
