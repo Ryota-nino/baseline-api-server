4e5dc9fda88377e408cba9692322c29cf443778c
K-shir0 <kakishiro2190394@gmail.com>
2020-11-25 09:12:07 +0900

    ユーザー登録機能のValidationを作成

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'student_number' => 'required|integer|digits:7',
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'sex' => 'required|integer|between:0,2', // 0:男, 1:女, 2:その他
+            'annual' => 'required|integer|min:1',
+            'year_of_graduation' => 'required|date',
+            'desired_occupations' => 'required|exists:occupational_categories.id',
+        ];
+    }
+}
