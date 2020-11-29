55ca3301ebe5f6cea012f02a40063fbb4642d5d5
K-shir0 <kakishiro2190394@gmail.com>
2020-11-29 19:50:46 +0900

    my_activitiesのデータベース構造を変更

--- database/migrations/2020_11_29_104034_create_my_activities_table.php
+++ database/migrations/2020_11_29_104034_create_my_activities_table.php
@@ -15,12 +15,14 @@ class CreateMyActivitiesTable extends Migration
     {
         Schema::create('my_activities', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('company_information_id')->unsigned();
             $table->text('content');
             $table->bigInteger('posted_year');
-            $table->bigInteger('posted_by')->unsigned();
             $table->timestamps();
 
-            $table->foreign('posted_by')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreign('company_information_id')->references('id')->on('company_information')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
         });
     }
 
