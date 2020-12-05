933ee4a3574cef9d4d5e1b0cfbe0873d28f7b2b8
Ryota-nino <ryota.swimmer@icloud.com>
2020-12-05 20:02:23 +0900

    登録に失敗したら登録前までロールバックするように修正

--- app/Http/Controllers/Interview/RegistInterviewController.php
+++ app/Http/Controllers/Interview/RegistInterviewController.php
@@ -8,8 +8,8 @@ use App\Http\Requests\InterviewRequest;
 use App\Models\CompanyInformation;
 use App\Models\Interview;
 use App\Models\InterviewContent;
-use Faker\Provider\ar_JO\Internet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RegistInterviewController extends Controller
 {
@@ -25,44 +25,39 @@ class RegistInterviewController extends Controller
         $user = Auth::user();
         $company_info = new CompanyInformation();
         $status = 200;
-        $message1 = 'Company_Information table is OK';
-        $message2 = 'Interview table is OK';
 
-        $company_info->user_id = $user->id;
-        if (!$company_info->fill($request->all())->save()) {
+        try{
+            DB::beginTransaction();
+            $company_info->user_id = $user->id;
+            $company_info->fill($request->all())->save();
+
+            foreach ($request->items as $item) {
+                $interview = new Interview();
+                $interview->fill($item);
+                $interview->company_information_id = $company_info->id;
+                $interview->step = $step;
+
+                $interview->save();
+
+                foreach ($item['contents'] as $content) {
+                    $interview_content = new InterviewContent();
+                    $interview_content->content = $content;
+                    $interview_content->interview_id = $interview->id;
+
+                    $interview_content->save();
+                }
+                $step++;
+                return response()->json([
+                    'message' => 'OK'
+                ], $status,);
+            }  
+        } catch (\Exception $e) {
+            DB::rollBack();
             $status = 400;
-            $message1 = 'Company_Information table is Bad Request';
+            return response()->json([
+                'message' => 'Failed to request'
+            ], $status,);
         }
         
-        foreach($request->items as $item){
-            $interview = new Interview();
-            $interview->fill($item);
-            $interview->company_information_id = $company_info->id;
-            $interview->step = $step;
-
-            if(!$interview->save()){
-                $status = 400;
-                $message2 = 'Interview table is Bad Request';
-            }
-
-            foreach ($item['contents'] as $content) {
-                $interview_content = new InterviewContent();
-                $interview_content->content = $content;
-                $interview_content->interview_id = $interview->id;
-
-                if (!$interview_content->save()) {
-                    $status = 400;
-                    $message2 = 'InterviewContents table is Bad Request';
-                }
-            }
-            $step++;
-        }
-
-         return response()->json([
-             'message' => [
-                $message1,
-                $message2
-            ]
-        ], $status, );
     }
 }
