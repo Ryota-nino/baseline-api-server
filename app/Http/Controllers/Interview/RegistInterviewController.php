807dd360133214f423bbb421f98abe3bea52aedb
Ryota-nino <ryota.swimmer@icloud.com>
2020-12-05 11:16:26 +0900

    Controllerの作成

---
+++ app/Http/Controllers/Interview/RegistInterviewController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Interview;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegistInterviewController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        
+    }
+}
