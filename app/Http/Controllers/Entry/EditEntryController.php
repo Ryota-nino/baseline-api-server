6a1e2bac815702c650c86b0331b4ade0792cee1c
Ryota-nino <ryota.swimmer@icloud.com>
2020-12-05 16:34:54 +0900

    紐づいていないidを指定した時、404で返すように修正

--- app/Http/Controllers/Entry/EditEntryController.php
+++ app/Http/Controllers/Entry/EditEntryController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Entry;
 
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\EntryRequest;
+use App\Http\Requests\EditEntryRequest;
 use App\Models\Entry;
+use App\Models\CompanyInformation;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class EditEntryController extends Controller
@@ -15,21 +18,35 @@ class EditEntryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke($id, Request $request)
+    public function __invoke($id, EditEntryRequest $request)
     {
-        $request->validate([
-            'title' => 'required',
-            'content' => 'required',
-        ]);
+        $company_info = CompanyInformation::query()->findOrFail($id);
+        $entry = CompanyInformation::with('entries')->findOrFail($id);
+        if($entry->entries->count() == 0){
+            abort(404);
+        }
+        $user = Auth::user();
+        $status = 200;
+        $company_info->user_id = $user->id;
+        
+        try{
+            DB::beginTransaction();
+            $company_info->fill($request->all())->update();
+            Entry::query()->where('company_information_id', $id)->delete();
 
-        $entry = Entry::findOrfail($id);
-        $entry->title = $request->title;
-        $entry->content = $request->content;
-        $entry->update();
+            foreach($request->items as $item){
+                $entry = new Entry();
+                $entry->fill($item);
+                $entry->company_information_id = $id;
+                $entry->save();
+            }
+            DB::commit();
+        }catch (\Exception $e){
+            DB::rollback();
+        }
 
         return response()->json([
             'message' => 'OK',
-            'data' => $entry
-        ], 200);
+        ], $status);
     }
 }
