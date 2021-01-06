6a74f34b03a288be7d49204e554d02e8cd7363a8
K-shir0 <kakishiro2190394@gmail.com>
2021-01-06 09:33:27 +0900

    パスワードをリセット用のコントローラを作成

---
+++ app/Http/Controllers/User/PasswordResetController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class PasswordResetController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
