8007d372fa1a0c11ec29a49ddb2603e546c5ddcc
K-shir0 <kakishiro2190394@gmail.com>
2021-01-06 18:08:05 +0900

    パスワードリセット処理とパスワードリセット用のメールを送る処理に切り分けた。

--- app/Http/Controllers/User/PasswordResetController.php
+++ app/Http/Controllers/User/PasswordResetController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use App\Models\PasswordReset;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Ramsey\Uuid\Uuid;
 
 class PasswordResetController extends Controller
 {
@@ -18,23 +16,6 @@ class PasswordResetController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //メールからユーザを取り出す処理
-        $this->validate($request, ['email' => 'required|email']);
 
-        // トークンの作成
-        // なければ作成
-        $password_reset = PasswordReset::query()
-            ->where('email', '=', $request->email)
-            ->firstOrCreate(
-                ['email' => $request->email]
-            );
-
-        $password_reset->fill(['token' => Uuid::uuid4(), 'created_at' => now()]);
-        $password_reset->save();
-
-        //TODO メールを送る
-
-
-        //TODO　
     }
 }
