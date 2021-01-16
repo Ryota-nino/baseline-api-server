0826bde7d95cea714733c3912d67750811058473
Ryota-nino <ryota.swimmer@icloud.com>
2021-01-16 10:28:01 +0900

    入社予定登録用ファイルの作成

---
+++ app/Http/Controllers/EmploymentStatus/EditDecisionOfferController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\EmploymentStatus;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EditDecisionOfferController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
