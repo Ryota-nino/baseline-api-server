39aaf597d42129bc118df7c9deec746ae45ea3cf
Ryota-nino <ryota.swimmer@icloud.com>
2021-01-26 14:38:16 +0900

    管理者アカウントだけ作成

--- database/seeders/DeploySeeder.php
+++ database/seeders/DeploySeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DeploySeeder extends Seeder
 {
@@ -13,6 +16,21 @@ class DeploySeeder extends Seeder
      */
     public function run()
     {
+        User::factory()->make([
+            'student_number' => 1000000,
+            'first_name' => 'アドミン',
+            'last_name' => 'ララベル',
+            'sex' => 2,
+            'annual' => 4,
+            'year_of_graduation' => 21,
+            'icon_image_path' => 'storage/example_user_icon.jpg',
+            'desired_occupations' => 1,
+            'privilege' => 2,
+            'email' => 'laravel-a@example.com',
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ])->save();
+
         $this->call(PrefectureTableSeeder::class);
         $this->call(InternshipSeeder::class);
         $this->call(OccupationalCategoriesSeeder::class);
